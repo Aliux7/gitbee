4d8b95ff989943f500a45f5693b504e7dc17a5d1
Aliux7 <kelsonedbert@gmail.com>
2024-10-10 18:26:43 +0700

    feats: dosen page on progress

--- src/app/(pages)/course-lecturer/page.tsx
+++ src/app/(pages)/course-lecturer/page.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion, useScroll } from "framer-motion";
 import { FaEye } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import Link from "next/link";
+import { RiStarSFill } from "react-icons/ri";
 
 import {
   Breadcrumb,
@@ -11,6 +15,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { RiStarSLine } from "react-icons/ri";
 
 import {
   Table,
@@ -24,13 +29,17 @@ import {
 import PopUpInsert from "@/app/components/course/PopUpInsert";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { SiGithub } from "react-icons/si";
+import { BsGlobe2 } from "react-icons/bs";
+import PreviewDetailComponent from "@/app/components/course-lecturer/PreviewDetailComponent";
 
 const page = () => {
   const { scrollYProgress } = useScroll();
   const prevScrollY = useRef(0);
   const [expand, setExpand] = useState(true);
-  const [showInsertForm, setShowInsertForm] = useState(false);
   const looping = [1, 2, 3, 4];
+  const [showPreviewDetailProject, setShowPreviewDetailProject] =
+    useState(false);
 
   useEffect(() => {
     scrollYProgress.onChange((currentScrollY) => {
@@ -50,7 +59,7 @@ const page = () => {
   return (
     <motion.div className="relative min-h-screen flex flex-col justify-start items-center px-[6.25rem] ">
       <div
-        className={`fixed top-0 w-full flex justify-center items-center transition-all ease-in-out duration-300 px-24 z-10 ${
+        className={`fixed top-0 w-full flex justify-center items-center transition-all ease-in-out duration-300 px-24 z-10 pb-5 ${
           expand ? "pt-24" : "pt-10"
         } bg-gray-50`}
       >
@@ -73,10 +82,24 @@ const page = () => {
                 </BreadcrumbItem>
                 <BreadcrumbSeparator />
                 <BreadcrumbItem>
-                  <BreadcrumbPage className="text-primary-orange">
+                  <BreadcrumbPage
+                    className={`${
+                      showPreviewDetailProject
+                        ? "text-primary-binus"
+                        : "text-primary-orange"
+                    }`}
+                  >
                     B601 - Software Engineering
                   </BreadcrumbPage>
                 </BreadcrumbItem>
+                {showPreviewDetailProject && <BreadcrumbSeparator />}
+                {showPreviewDetailProject && (
+                  <BreadcrumbItem>
+                    <BreadcrumbPage className="text-primary-orange">
+                      Preview Group 1
+                    </BreadcrumbPage>
+                  </BreadcrumbItem>
+                )}
               </BreadcrumbList>
             </Breadcrumb>
           </div>
@@ -96,136 +119,197 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="h-fit w-full pt-40 pb-10 flex flex-col gap-5">
-        <div className="">
-          <h1 className="">
-            <span className="text-primary-binus font-semibold">Class:</span>{" "}
-            BG01
-          </h1>
-          <h1 className="">
-            <span className="text-primary-binus font-semibold">Course:</span>{" "}
-            COMP6100001 - Software Engineering
-          </h1>
-          <h1 className="">
-            <span className="text-primary-binus font-semibold">
-              Start Date Correction:
-            </span>{" "}
-            20 Sept 2024
-          </h1>
-          <h1 className="">
-            <span className="text-primary-binus font-semibold">
-              End Date Correction:
-            </span>{" "}
-            20 Sept 2024
-          </h1>
-          <h1 className="">
-            <span className="text-primary-binus font-semibold">Status:</span>{" "}
-            <span className="text-red-500 ">Not Yet Graded</span>
-          </h1>
-        </div>
-        <div className="grid grid-cols-3 gap-5 justify-center items-start w-full place-items-stretch">
-          {looping.map((index) => (
-            <div className="w-full h-fit overflow-y-auto shadow-xl border rounded-xl p-5">
-              <div className="w-full flex justify-between items-center">
-                <h1 className="text-xl">Group {index}</h1>
-                <button className="bg-primary-binus text-white px-2 py-1 rounded-md flex justify-center items-center gap-1">
-                  <FaEye fill="white"/> Preview
-                </button>
-              </div>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="">NO</TableHead>
-                    <TableHead className="">NIM</TableHead>
-                    <TableHead className="">NAME</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">1</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell className=" truncate">
-                      Kelson Edbert Susilo
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">2</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell>
-                      <h1 className=" truncate">Kelson Edbert Susilo</h1>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">3</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell>
-                      <h1 className=" truncate">Kelson Edbert Susilo</h1>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">4</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell>
-                      <h1 className=" truncate">Kelson Edbert Susilo</h1>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-              <div className="w-full flex justify-end items-center gap-5 border-t pt-3">
-                <Label className="text-lg">Score:</Label>
-                <Input type="number" max={100} min={0} className="w-auto" />
-              </div>
+      {!showPreviewDetailProject && (
+        <div className="h-fit w-full pt-36 pb-10 flex flex-col gap-5">
+          <div className="flex justify-between items-end">
+            <div>
+              <h1 className="">
+                <span className="text-primary-binus font-semibold">Class:</span>{" "}
+                BG01
+              </h1>
+              <h1 className="">
+                <span className="text-primary-binus font-semibold">
+                  Course:
+                </span>{" "}
+                COMP6100001 - Software Engineering
+              </h1>
+              <h1 className="">
+                <span className="text-primary-binus font-semibold">
+                  Start Date Correction:
+                </span>{" "}
+                20 Sept 2024
+              </h1>
+              <h1 className="">
+                <span className="text-primary-binus font-semibold">
+                  End Date Correction:
+                </span>{" "}
+                20 Sept 2024
+              </h1>
+              <h1 className="">
+                <span className="text-primary-binus font-semibold">
+                  Status:
+                </span>{" "}
+                <span className="text-red-500 ">Not Yet Graded</span>
+              </h1>
             </div>
-          ))} 
-          {looping.map((index) => (
-            <div className="w-full h-fit overflow-y-auto shadow-xl border rounded-xl p-5">
-              <div className="w-full flex justify-between items-center">
-                <h1 className="text-xl">Group {index}</h1>
-                <button className="bg-primary-binus text-white px-2 py-1 rounded-md flex justify-center items-center gap-1">
-                  <FaEye fill="white"/> Preview
-                </button>
-              </div>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="">NO</TableHead>
-                    <TableHead className="">NIM</TableHead>
-                    <TableHead className="">NAME</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">1</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell className=" truncate">
-                      Kelson Edbert Susilo
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">2</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell>
-                      <h1 className=" truncate">Kelson Edbert Susilo</h1>
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell className="font-medium text-center">3</TableCell>
-                    <TableCell className="">2540115465</TableCell>
-                    <TableCell>
-                      <h1 className=" truncate">Kelson Edbert Susilo</h1>
-                    </TableCell>
-                  </TableRow> 
-                </TableBody>
-              </Table>
-              <div className="w-full flex justify-end items-center gap-5 border-t pt-3">
-                <Label className="text-lg">Score:</Label>
-                <Input type="number" max={100} min={0} className="w-auto" />
-              </div>
+            <div>
+              <button className="bg-purple-600 text-white px-4 py-2 rounded-md flex justify-center items-center gap-2">
+                <FaCheck fill="white" /> Finalized
+              </button>
             </div>
-          ))} 
+          </div>
+          <div className="grid grid-cols-3 gap-5 justify-center items-start w-full place-items-stretch">
+            {looping.map((index) => (
+              <div className="w-full h-fit overflow-y-auto shadow-xl border rounded-xl p-5">
+                <div className="w-full flex justify-between items-center">
+                  <h1 className="text-xl">Group {index}</h1>
+                  <button
+                    className="bg-primary-binus text-white px-2 py-1 rounded-md flex justify-center items-center gap-1"
+                    onClick={() => setShowPreviewDetailProject(true)}
+                  >
+                    <FaEye fill="white" /> Preview
+                  </button>
+                </div>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="">NO</TableHead>
+                      <TableHead className="">NIM</TableHead>
+                      <TableHead className="">NAME</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        1
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell className=" truncate">
+                        Kelson Edbert Susilo
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        2
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell>
+                        <h1 className=" truncate">Kelson Edbert Susilo</h1>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        3
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell>
+                        <h1 className=" truncate">Kelson Edbert Susilo</h1>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        4
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell>
+                        <h1 className=" truncate">Kelson Edbert Susilo</h1>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+                <div className="w-full flex justify-between items-end gap-5 border-t pt-3">
+                  <div className="w-1/2">
+                    <Label className="text-lg">Score:</Label>
+                    <Input
+                      type="number"
+                      max={100}
+                      min={0}
+                      className="w-auto"
+                      placeholder="0 - 100"
+                    />
+                  </div>
+                  {/* <div className="w-1/2 flex justify-end items-center text-gray-500 gap-2">
+                    <RiStarSFill
+                      fill="white"
+                      stroke="#6b7280"
+                      style={{ strokeWidth: "2" }}
+                    />
+                    Not Recommended
+                  </div> */}
+                  <div className="w-1/2 flex justify-end items-center text-yellow-500 gap-2 cursor-pointer">
+                    <RiStarSFill
+                      fill="#eab308"
+                      stroke="#eab308"
+                      style={{ strokeWidth: "2" }}
+                    />
+                    Recommended
+                  </div>
+                </div>
+              </div>
+            ))}
+            {looping.map((index) => (
+              <div className="w-full h-fit overflow-y-auto shadow-xl border rounded-xl p-5">
+                <div className="w-full flex justify-between items-center">
+                  <h1 className="text-xl">Group {index}</h1>
+                  <button className="bg-primary-binus text-white px-2 py-1 rounded-md flex justify-center items-center gap-1">
+                    <FaEye fill="white" /> Preview
+                  </button>
+                </div>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="">NO</TableHead>
+                      <TableHead className="">NIM</TableHead>
+                      <TableHead className="">NAME</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        1
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell className=" truncate">
+                        Kelson Edbert Susilo
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        2
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell>
+                        <h1 className=" truncate">Kelson Edbert Susilo</h1>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        3
+                      </TableCell>
+                      <TableCell className="">2540115465</TableCell>
+                      <TableCell>
+                        <h1 className=" truncate">Kelson Edbert Susilo</h1>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+                <div className="w-full flex justify-end items-center gap-5 border-t pt-3">
+                  <Label className="text-lg">Score:</Label>
+                  <Input type="number" max={100} min={0} className="w-auto" />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      {showInsertForm && <PopUpInsert setShowInsertForm={setShowInsertForm} />}
+      )}
+      {showPreviewDetailProject && (
+        <div className="h-fit w-full pt-40 pb-10 flex flex-col gap-5">
+          <PreviewDetailComponent
+            setShowPreviewDetailProject={setShowPreviewDetailProject}
+            expand={expand}
+          />
+        </div>
+      )}
     </motion.div>
   );
 };
---
+++ src/app/components/course-lecturer/PreviewDetailComponent.tsx
@@ -0,0 +1,125 @@
+import Link from "next/link";
+import React from "react";
+import { BsGlobe2 } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { SiGithub } from "react-icons/si";
+
+interface PreviewDetailProps {
+  setShowPreviewDetailProject: (value: boolean) => void;
+  expand: boolean;
+}
+
+function PreviewDetailComponent(props: PreviewDetailProps) {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
+
+  return (
+    <motion.div
+      className="relative flex justify-center items-start w-auto bg-white h-full rounded-md px-5 mb-14 pb-7 pt-3"
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      <motion.div
+        className={`sticky ${
+          props.expand ? "top-40" : "top-24"
+        } w-[30rem] h-full py-2 opacity-100 flex flex-col gap-5 transition-all ease-in-out duration-500 overflow-hidden`}
+        variants={containerVariants}
+      >
+        {/* <h1 className="text-center py-10 text-gray-500">No Project Selected</h1> */}
+        <Link
+          href={"/portofolio/123"}
+          className="flex justify-start items-center gap-5 border-b pb-5 mr-4 cursor-pointer"
+        >
+          <img
+            src="/images/1.jpg"
+            className="rounded-full h-20 w-20 p-1 border object-cover"
+          />
+          <div className="w-48">
+            <h1 className="truncate font-semibold">Kelson Edbert S</h1>
+            <h1 className="truncate text-sm text-gray-500">2540115465</h1>
+            <h1 className="truncate text-sm text-gray-500">Computer Science</h1>
+          </div>
+        </Link>
+        <Link
+          href={"/portofolio/123"}
+          className="flex justify-start items-center gap-5 border-b pb-5 mr-4 cursor-pointer"
+        >
+          <img
+            src="/images/3.jpg"
+            className="rounded-full h-20 w-20 p-1 border object-cover"
+          />
+          <div className="w-48">
+            <h1 className="truncate font-semibold">Timothy Darren</h1>
+            <h1 className="truncate text-sm text-gray-500">2540115465</h1>
+            <h1 className="truncate text-sm text-gray-500">Computer Science</h1>
+          </div>
+        </Link>
+      </motion.div>
+      <motion.div
+        className="relative w-full border-l pl-3 flex h-fit py-3 justify-start items-start transition-all ease-in-out duration-500"
+        variants={containerVariants}
+      >
+        <div
+          className="hover:bg-gray-100 p-1.5 cursor-pointer rounded-full"
+          onClick={() => props.setShowPreviewDetailProject(false)}
+        >
+          <IoIosArrowRoundBack className="w-7 h-7" />
+        </div>
+        <div className="w-full flex flex-col pr-5">
+          <div className="w-full flex justify-start items-start border-b pb-5">
+            <div className="mx-3 flex flex-col gap-1 w-2/3">
+              <h1 className="text-3xl font-bold">The Spotify</h1>
+              <h3 className="text-sm text-gray-500">
+                By Kelson Edbert S, Timothy Darren, Nicholas Chandra
+              </h3>
+              <div className="h-fit flex-grow my-3 pr-10">
+                <h1 className="text-balance text-gray-700">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+                  molestias possimus ipsum? Fuga architecto, ipsum nulla
+                  explicabo quas corrupti quia labore eum dolor ipsam obcaecati
+                  facere odio aliquam aspernatur perferendis. Lorem ipsum, dolor
+                  sit amet consectetur adipisicing elit. Veniam corrupti quod
+                  eos nulla vero debitis corporis ullam, earum laudantium
+                  praesentium asperiores ipsum ab voluptatem molestias atque
+                  mollitia quidem sint in!
+                </h1>
+              </div>
+              <Link
+                href="https://github.com/Aliux7/gitbee"
+                className="flex justify-start items-center gap-2 text-sm my-1 text-primary-binus"
+              >
+                <SiGithub fill="#EB9327" />
+                https://github.com/Aliux7/gitbee
+              </Link>
+              <Link
+                href="https://binusmaya.binus.ac.id/"
+                className="flex justify-start items-center gap-2 text-sm my-1 text-primary-binus"
+              >
+                <BsGlobe2 fill="#EB9327" /> https://binusmaya.binus.ac.id/
+              </Link>
+            </div>
+            <div className="w-1/3">
+              {/* <img
+                    src="/images/image-1.webp"
+                    className="w-full rounded-md"
+                    /> */}
+              <img src="/images/3.jpg" className="w-full rounded-md border" />
+            </div>
+          </div>
+          <div className="w-full h-96 my-3 flex overflow-auto gap-3">
+            <img src="/images/1.jpg" className="h-full rounded-md border" />
+            <img src="/images/2.jpg" className="h-full rounded-md border" />
+            <img src="/images/3.jpg" className="h-full rounded-md border" />
+            <img src="/images/4.jpg" className="h-full rounded-md border" />
+          </div>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export default PreviewDetailComponent;
